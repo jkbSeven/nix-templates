da2e960ce4cddeddb7c96085cc43bb60f779e353
jkb <Jacob202@protonmail.com>
2026-02-22 23:11:28 +0100

    fix: included the c template in the topmost flake

--- flake.nix
+++ flake.nix
@@ -9,6 +9,10 @@
                 pypoetry_manual = {
                     path = ./pypoetry_manual;
                 };
+
+                c = {
+                    path = ./c
+                };
             };
     };
 
