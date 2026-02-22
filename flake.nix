0d7868ff6b15e4562d0dfa5d612a8e6d9b5a6849
jkb <Jacob202@protonmail.com>
2026-02-22 23:19:02 +0100

    feat: added descriptions to each flake template in the main flake

--- flake.nix
+++ flake.nix
@@ -8,10 +8,12 @@
             templates = {
                 pypoetry_manual = {
                     path = ./pypoetry_manual;
+                    description = "Dev env with python and poetry, where poetry manages the dependencies";
                 };
 
                 c = {
                     path = ./c;
+                    description = "Basic C dev env with gcc and clang";
                 };
             };
     };
